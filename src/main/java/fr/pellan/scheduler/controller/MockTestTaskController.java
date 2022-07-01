7fbe76c14f026e853fb0307b358675d85800da7e
Thomas <thomas.pellan@gmail.com>
2022-07-01 15:17:41 +0200

    fix(api) cors config for frontend usage

--- src/main/java/fr/pellan/scheduler/controller/MockTestTaskController.java
+++ src/main/java/fr/pellan/scheduler/controller/MockTestTaskController.java
@@ -2,18 +2,22 @@ package fr.pellan.scheduler.controller;
 
 import com.google.gson.Gson;
 import fr.pellan.scheduler.dto.MockTestTaskControllerDTO;
+import fr.pellan.scheduler.entity.ScheduledTaskOutputEntity;
+import fr.pellan.scheduler.service.ScheduledTaskOutputService;
 import fr.pellan.scheduler.task.TaskResultResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  * Mock controller used to simulate a remote task execution and it's return value.
@@ -23,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("test")
 public class MockTestTaskController {
+
+    private static final double RANDOM_FAIL = 0.3;
 
     /**
      * Prints the data received and sends back a simple response.
@@ -35,15 +41,16 @@ public class MockTestTaskController {
     @ApiResponse(responseCode = "200", description = "call succeeded, see body for response")
     private ResponseEntity<String> helloWorld(@RequestBody MockTestTaskControllerDTO data){
 
-        if(data != null && !StringUtils.isBlank(data.getTestData())){
-            log.info("helloWorld : got data from scheduler : {}", data.getTestData());
+        if(Math.random() < RANDOM_FAIL){
+
+            return new ResponseEntity(null, HttpStatus.BAD_REQUEST);
         }
 
         TaskResultResponse response = TaskResultResponse.builder()
-                .success(true)
-                .data("Oh hi Mark !!")
-                .error("Nothing serious was broken")
-                .build();
+            .success(true)
+            .data(data.getTestData())
+            .error("Nothing serious was broken")
+            .build();
 
         return new ResponseEntity(new Gson().toJson(response), HttpStatus.OK);
     }
